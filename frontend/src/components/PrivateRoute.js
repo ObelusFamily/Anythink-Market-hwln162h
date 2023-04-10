fd9669b3461f8b6b88631727fd491cf13d3d49a5
John Rusch <35750236+johnrusch@users.noreply.github.com>
2023-04-10 23:39:07 +0000

    this is dumb

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -5,7 +5,7 @@ import { Route, Redirect } from 'react-router-dom';
 const PrivateRoute = ({ children, currentUser, ...rest }) => {
   return (
     <Route {...rest} render={({ location }) =>
-    currentUser ? (
+    currentUser !== null ? (
       children
     ) : (
       <Redirect
